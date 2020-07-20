6e01763260308a1c7d1952a9a3ea01d8a5511b0b
Stormy Main Linux <raphael.beltrand@gmail.com>
2020-07-20 23:58:49 +0200

    fix http link

--- app/javascript/src/apolloClient.js
+++ app/javascript/src/apolloClient.js
@@ -1,21 +1,22 @@
 import Cookies from "universal-cookie";
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-
-const cache = new InMemoryCache();
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const cookies = new Cookies();
 
-const token = localStorage.getItem("token");
-
-let loaderCount = 0;
 const apolloClient = new ApolloClient({
-  cache,
-  link: new HttpLink({
+  cache: new InMemoryCache(),
+  link: createHttpLink({
     uri: "/graphql",
-    headers: {
-      authorization: `Scritcher ${cookies.get("csrf-token")}`,
-      "X-CSRF-Token": cookies.get("csrf-token"),
-    },
+    headers: setContext((_, { headers }) => {
+      return {
+        headers: {
+          ...headers,
+          authorization: `Scritcher ${cookies.get("csrf-token")}`,
+          "X-CSRF-Token": cookies.get("csrf-token"),
+        },
+      };
+    }),
   }),
 });
 
