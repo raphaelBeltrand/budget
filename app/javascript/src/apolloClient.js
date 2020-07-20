c8d808c01dc5bf528ddd921e6092a08ddb10b987
Stormy Main Linux <raphael.beltrand@gmail.com>
2020-07-20 20:47:50 +0200

    fix http link

--- app/javascript/src/apolloClient.js
+++ app/javascript/src/apolloClient.js
@@ -11,35 +11,12 @@ let loaderCount = 0;
 const apolloClient = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: "https://ezpzbudget.com/graphql",
+    uri: "/graphql",
     headers: {
       authorization: `Scritcher ${cookies.get("csrf-token")}`,
       "X-CSRF-Token": cookies.get("csrf-token"),
     },
-    credentials: "same-origin",
   }),
-  fetch: (input, init) => {
-    // this whole function is pretty ugly
-
-    const globalProgresses = document.querySelectorAll(".globalProgress");
-    loaderCount++;
-    Array.from(globalProgresses).forEach((globalProgress) => {
-      globalProgress.style.display = "block";
-    });
-
-    const handleResponse = (response) => {
-      loaderCount--;
-
-      if (loaderCount === 0) {
-        Array.from(globalProgresses).forEach((globalProgress) => {
-          globalProgress.style.display = "none";
-        });
-      }
-      return response;
-    };
-
-    return fetch(input, init).then(handleResponse).catch(handleResponse);
-  },
 });
 
 export default apolloClient;
