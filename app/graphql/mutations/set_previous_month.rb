6b8aa219d9977767effbaad06b02be5ba9bb7659
Stormy Main Linux <raphael.beltrand@gmail.com>
2020-07-20 16:49:30 +0200

    fix typo in date mutation

--- app/graphql/mutations/set_previous_month.rb
+++ app/graphql/mutations/set_previous_month.rb
@@ -3,7 +3,12 @@ class Mutations::SetPreviousMonth < Mutations::BaseMutation
   
     def resolve(params)
         current_user = context[:current_user]
-        current_user.selected_month = if (current_user.selected_month == 1) 12 else current_user.selected_month - 1 end
+        current_user.selected_month = 
+            if (current_user.selected_month == 1) 
+                12 
+            else 
+                current_user.selected_month - 1 
+            end
         if current_user.selected_month == 12
             current_user.selected_year = current_user.selected_year - 1
         end
