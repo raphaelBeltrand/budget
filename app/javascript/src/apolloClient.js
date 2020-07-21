95bea048c1c87bd7b5b3b2d147d0ea31542f69a7
Stormy Main Linux <raphael.beltrand@gmail.com>
2020-07-21 08:28:12 +0200

    fix http link

--- app/javascript/src/apolloClient.js
+++ app/javascript/src/apolloClient.js
@@ -3,21 +3,22 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const cookies = new Cookies();
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Scritcher ${cookies.get("csrf-token")}`,
+      "X-CSRF-Token": cookies.get("csrf-token"),
+    },
+  };
+});
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  link: createHttpLink({
-    uri: "/graphql",
-    headers: setContext((_, { headers }) => {
-      return {
-        headers: {
-          ...headers,
-          authorization: `Scritcher ${cookies.get("csrf-token")}`,
-          "X-CSRF-Token": cookies.get("csrf-token"),
-        },
-      };
-    }),
-  }),
+  link: authLink.concat(httpLink),
 });
 
 export default apolloClient;
