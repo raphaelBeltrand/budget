ffb3266185a2bbbb86bfc50a15f276811742244c
Stormy Main Linux <raphael.beltrand@gmail.com>
2020-07-22 15:21:23 +0200

    try to fix array issue in rec model 2

--- app/models/recurrent_entry.rb
+++ app/models/recurrent_entry.rb
@@ -4,11 +4,6 @@ class RecurrentEntry < ApplicationRecord
     belongs_to :parent_entry, optional: true
     belongs_to :user
 
-    # Predicate to check if recurring entry is within the range of given month and year
-    scope :within_time_range, -> (variables) { 
-        where("(start_year < :year OR (start_year = :year AND start_month <= :month)) AND (end_year > :year OR (end_year = :year AND end_month >= :month))", variables)
-    }
-
     # Predicate to check if recurring entry is on the periodicity range of the given month and year
-    scope :on_period, -> (month) { where("(MOD(start_month - ?, periodicity) = 0)", month)}
+    scope :on_period, -> (month) { where("(MOD(start_month - ?, periodicity) = 0)", month) }
 end
--- app/services/budget_calculator.rb
+++ app/services/budget_calculator.rb
@@ -1,6 +1,10 @@
 class BudgetCalculator
     attr_accessor :current_user
+    attr_accessor :within_time_range_predicate
     current_user = nil
+
+    # Predicate to check if recurring entry is within the range of given month and year
+    within_time_range_predicate = "(start_year < :year OR (start_year = :year AND start_month <= :month)) AND (end_year > :year OR (end_year = :year AND end_month >= :month))"
 
     def initialize(args)
         current_user = args[:current_user]
@@ -38,7 +42,7 @@ class BudgetCalculator
     # Compute the total incoming money from recurring and one time entries on the current loop month
     def compute_income(year, month)
         RecurrentEntry.where(kind: "positive", user: current_user)
-                .within_time_range({year: year, month: month})
+                .where(within_time_range_predicate, {year: year, month: month})
                 .on_period(month)
                 .pluck(:value).sum +
             OneTimeEntry.where(kind: "positive", user: current_user).where("year = ? AND month = ?", year, month).pluck(:value).sum
@@ -47,7 +51,7 @@ class BudgetCalculator
     # Compute the total outgoing money from recurring and one time entries on the current loop month
     def compute_outcome(year, month)
         RecurrentEntry.where(kind: "negative", user: current_user)
-                .within_time_range({year: year, month: month})
+                .where(within_time_range_predicate, {year: year, month: month})
                 .on_period(month)
                 .pluck(:value).sum +
             OneTimeEntry.where(kind: "negative", user: current_user).where("year = ? AND month = ?", year, month).pluck(:value).sum
